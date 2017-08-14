187be9d11d25ada20299d1c7ecdd0f5dbd6b7e42
csp5096 <csp5096@yahoo.com>
2017-08-13 21:10:33 -0500

    Books 1

--- src/catalog/urls.py
+++ src/catalog/urls.py
@@ -6,5 +6,8 @@ from catalog import views
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view(), name='home'),
     url(r'^about/$', views.AboutPageView.as_view(), name='about'),
-    url(r'^catalog/$', views.CatalogPageView.as_view(), name='catalog'),
+    url(r'^books/$', views.BookListView.as_view(), name='books'),
+    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
+    url(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
--- src/catalog/views.py
+++ src/catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView, RedirectView
+from django.views import generic
+from .models import Book, Author, BookInstance, Genre
 # Import TemplateView
 
 class HomePageView(TemplateView):
@@ -8,9 +10,42 @@ class HomePageView(TemplateView):
 class AboutPageView(TemplateView):
     template_name = "about.html"
 
-class CatalogPageView(TemplateView):
-    template_name = "primes/catalog.html"
+class BookListView(generic.ListView):
+    template_name = "catalog/book.html"
+    model = Book
+    context_object_name = 'book_list' # your own name for the list as a template variable
+    paginate_by = 2
 
+    def get_queryset(self):
+        return Book.objects.filter(title__icontains='a')[:5] # get 5 books containing the title 'The'
 
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super ( BookListView, self ).get_context_data ( **kwargs )
+        # Get the blog from id and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
 
+class BookDetailView(generic.DetailView):
+    template_name = "catalog/book_detail.html"
+    model = Book
 
+class AuthorListView(generic.ListView):
+    template_name = "catalog/author.html"
+    model = Author
+    context_object_name = 'author_list' # your own name for the list as a template variable
+    paginate_by = 2
+
+    def get_queryset(self):
+        return Author.objects.filter(first_name__icontains='a')[:5] # get 5 books containing the title 'The'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super ( AuthorListView, self ).get_context_data ( **kwargs )
+        # Get the blog from id and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
+
+class AuthorDetailView(generic.DetailView):
+    template_name = "catalog/author_detail.html"
+    model = Author
